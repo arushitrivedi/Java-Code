7763164b00d43dcec765693ac9fe70169d3ee045
Arushi Trivedi <arushi.19417@knit.ac.in>
2023-10-02 23:23:20 +0530

    Create Program15.java
    Solved gfg question.

---
+++ Program15.java
@@ -0,0 +1,21 @@
+//Number of distinct subsequences - GFG
+class Solution 
+{
+    int distinctSubsequences(String s) 
+  {
+         int mod = 1000000007;
+        int dp[] = new int[s.length()+1];
+        dp[0] = 1;
+        
+        int ch[] = new int[26];
+        
+        for(int i=1;i<dp.length;i++){
+            int val = ch[s.charAt(i-1)-'a'];
+            dp[i] = ((2 * dp[i-1])%mod) - val;
+            dp[i] %= mod;
+            ch[s.charAt(i-1) - 'a'] = dp[i-1];
+        }
+        int ans = (dp[s.length()]+mod)%mod;
+        return ans;// code here
+    }
+}
