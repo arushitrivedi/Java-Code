6e131a12033e7091333773b2d515a71e5661ab58
Arushi Trivedi <arushi.19417@knit.ac.in>
2023-10-31 22:53:01 +0530

    Create Program 45.java
    Added solution of a Leetcode problem.

---
+++ Program 45.java
@@ -0,0 +1,16 @@
+//LEETCODE : Find the original array
+class Solution
+ {
+    public int[] findArray(int[] pref) 
+    {
+        int prev = pref[0];
+        
+        for (int i = 1; i < pref.length; i++) {
+            pref[i] ^= prev;
+            prev ^= pref[i];
+        }
+        
+        return pref; 
+        
+    }
+}
